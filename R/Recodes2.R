e8e23e823d48cb31203b9259431c1b05f4f04d43
Ben Rogers <bjroger@ku.edu>
2015-06-26 15:38:33 -0500

    Edited Summary Tables a little

--- R/Recodes2.R
+++ R/Recodes2.R
@@ -12,3 +12,8 @@
 femdat <- read.csv("/Users/bjr/Desktop/School/WSFDat/femdat.csv")
 homdat <- read.csv("/Users/bjr/Desktop/School/WSFDat/homdat.csv")
 
+## Fix a problem w/Homdat$BirthYear
+
+as.character(homdat$BirthYear)
+homdat$BirthYear[!homdat$BirthYear %in% as.character(1900:1999)] <- NA
+homdat$BirthYear <- as.numeric(as.character(homdat$BirthYear))

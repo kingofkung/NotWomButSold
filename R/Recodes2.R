3f41f1b9fc26d848f76573567ad57ae59ea71b8a
Ben Rogers <bjroger@ku.edu>
2015-06-26 16:48:41 -0500

    Display ideas

--- R/Recodes2.R
+++ R/Recodes2.R
@@ -9,6 +9,8 @@
 ## Procead as needed                            ##
 ##################################################
 
+library(plyr)
+
 femdat <- read.csv("/Users/bjr/Desktop/School/WSFDat/femdat.csv")
 homdat <- read.csv("/Users/bjr/Desktop/School/WSFDat/homdat.csv")
 
@@ -17,3 +19,48 @@ homdat <- read.csv("/Users/bjr/Desktop/School/WSFDat/homdat.csv")
 as.character(homdat$BirthYear)
 homdat$BirthYear[!homdat$BirthYear %in% as.character(1900:1999)] <- NA
 homdat$BirthYear <- as.numeric(as.character(homdat$BirthYear))
+
+colnames(homdat)
+
+levels(homdat$WhySupportFemaleService)
+homdat$WhySupportFemaleService[homdat$WhySupportFemaleService == ""] <- NA
+
+
+## Combine the two datasets,
+
+homdat$female <- 0
+femdat$female <- 1
+
+datcomb <- rbind.fill(homdat, femdat)
+
+
+
+##' Test 1 2 3... Description goes here
+##'
+##' A function to make a single kind of response into NAs Details
+##' @param vec A single dimensional vector of data
+##' @param toNA The response we'd like to be an NA
+##' @return the vector in vec with toNA responses = NA
+##' @author Benjamin Rogers
+namaker <- function(vec, toNA = "" ){
+    vec[vec == toNA] <- NA
+    if( is.factor(vec) == T) vec <- factor(vec)
+    return(vec)
+}
+
+namaker(homdat$FemalesAreWhiny)
+
+homdatimp <- lapply(homdat, namaker)
+
+homdatimp <- lapply(homdatimp, namaker, toNA = "-")
+
+homdatimp$HarrassTrainingInsufficient
+
+table(datcomb$ServedCST)
+table(femdat$ServedCST)
+
+table(datcomb$HarrassTrainingInsufficient)
+table(femdat$HarrassTrainingInsufficient)
+table(homdat$HarrassTrainingInsufficient)
+
+
